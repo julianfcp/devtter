8802bc7a9194fe8cd8e196a52a2c4e3296d7151e
Julian <julian.fcp@gmail.com>
2021-04-24 20:25:16 -0500

    timeline Devits update

--- styles/Home-css.js
+++ styles/Home-css.js
@@ -3,13 +3,13 @@ import { motion } from "framer-motion";
 
 export const Main = styled(motion.main)`
   width: 500px;
-  border-radius: 10px 10px;
-  margin: 10px;
+  //border-radius: 10px 10px;
   box-shadow: #000 0px 2px 20px;
   flex: 1;
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  //border: 3px solid blue;
+  margin: 0px 0px;
   @media screen and (max-width: 600px) {
     width: 100%;
     margin: 0;
@@ -24,7 +24,7 @@ export const Header = styled(motion.header)`
   width: inherit;
   position: fixed;
   top: 0;
-  border: 1px solid black;
+  border 0.01em solid #cccccc;
   background-color: white;
 `;
 
@@ -34,9 +34,27 @@ export const Footer = styled(motion.footer)`
   height: 49px;
   position: fixed;
   bottom: 0;
-  border: 1px solid black;
+  border: 0.01em solid #cccccc;
 `;
 
 export const Section = styled(motion.section)`
   padding-top: 50px;
+  margin-top: 0px;
+`;
+
+export const Devit = styled(motion.div)`
+  border-bottom: 0.01em solid #cccccc;
+  margin: 2px;
+  display: flex;
+  align-items: flex-start;
+  margin: 0px;
+  padding: 10px;
+  h4 {
+    margin-top: 2px;
+    margin-bottom: 5px;
+  }
+`;
+export const DContent = styled(motion.div)`
+  margin: 0px 10px;
+  padding-top: 0px;
 `;
