9a6b1a43883e7723abbc42ab700bc71bd29a0a94
Julian <julian.fcp@gmail.com>
2021-05-30 14:14:01 -0500

    scrollbar

--- components/Devit/index.js
+++ components/Devit/index.js
@@ -7,13 +7,18 @@ import { Image } from "styles/Compose-css";
 import Avatar from "components/Avatar";
 import useTimeAgo from "hooks/useTimeAgo";
 
-const Devit = ({ avatar, img, userName, content, createdAt }) => {
+const Devit = ({ id, avatar, img, userName, content, createdAt }) => {
   const timeAgo = useTimeAgo(createdAt);
+
+  const handleDevit = () => {
+    console.log(id);
+  };
+
   return (
     <>
       <DevitStyled>
         <Avatar src={avatar} alt={userName}></Avatar>
-        <DevitContent>
+        <DevitContent onClick={handleDevit}>
           <DevitHead>
             <h4>{userName}</h4>
             <small>{timeAgo}</small>
